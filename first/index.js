95cc516497c80c0b1be7eae98838cd1814c908ef
season <huangxyk@yonyou.com>
2019-08-09 09:48:21 +0800

    垂直躲避怪物

--- first/index.js
+++ first/index.js
@@ -1,38 +1,3 @@
-// (function () {
-// 	// 准备资源
-// 	var context = document.getElementById('content').getContext('2d');
-// 	var heroImg = new Image();
-
-// 	// 画图
-// 	heroImg.onload = function () {
-// 		var imgPos = {
-// 			x: 0,
-// 			y: 0,
-// 			width: 32,
-// 			height: 32
-// 		};
-
-// 		var rect = {
-// 			x: 0,
-// 			y: 0,
-// 			width: 40,
-// 			height: 40
-// 		};
-
-// 		context.drawImage(
-// 			heroImg,
-// 			imgPos.x,
-// 			imgPos.y,
-// 			imgPos.width,
-// 			imgPos.height,
-// 			rect.x,
-// 			rect.y,
-// 			rect.width,
-// 			rect.height
-// 		);
-// 	};
-// 	heroImg.src = './hero.png';
-// })()
 
 (
 	function() {
@@ -89,29 +54,6 @@
 				rect.y
 			)
 		}
-
-
-
-		// var move = function() {
-		// 	// document.onkeydown = (function(e) {
-		// 	// // console.log('e.keyCode:', e.keyCode);
-		// 	// // 37, 38, 39 ,40 方向键值
-		// 	// console.log("hero this:", this);
-		// 	// console.log("hero window:", window);
-		// 	// // if (e.keyCode === 37) { // 向左移动
-		// 	// // 	rect.x =(this.rect.x - 40 > 0) ?  (this.rect.x - 40) : 0;
-		// 	// // }
-		// 	// // if (e.keyCode === 39) { // 向右移动
-		// 	// // 	rect.x = (this.rect.x + 40 > width) ? width : this.rect.x + 40;
-		// 	// // }
-		// 	// });
-		// 	window.addEventListener('keyup', function(e) {
-		// 		console.log('e.keyCode:', e.keyCode);
-		// 		if (e.keyCode === 39) { // 向右移动
-		// 			rect.x = (this.rect.x + 40 > width) ? width : this.rect.x + 40;
-		// 		}
-		// 	});
-		// }
 		
 		 let hero = {
 		 	img: heroImg,
@@ -143,7 +85,7 @@
 		 		height: 32
 		 	},
 		 	rect: {
-		 		x: 32,
+		 		x: 40,
 		 		y: 80,
 		 		width: 32,
 		 		height: 32
@@ -169,7 +111,7 @@
 			if (e.keyCode === 37) { // 向左移动
 				const leftX = hero.rect.x;
 				hero.rect.x =(leftX - heroSpace > 0) ? (leftX - heroSpace) : 0;
-				if ((hero.rect.y === monster.rect.y) && ((monster.rect.x < hero.rect.x) && (hero.rect.x < monster.rect.x + heroSpace))) { 
+				if ((hero.rect.y === monster.rect.y) && ((monster.rect.x <= hero.rect.x) && (monster.rect.x >= hero.rect.x))) { 
 					// 高度一样，要画的x轴为英雄+40 在怪物的区间内【32， 72】就不能重写
 					hero.rect.x = leftX;
 				}
@@ -177,19 +119,24 @@
 			if (e.keyCode === 39) { // 向右移动
 				let rightX = hero.rect.x;
 				hero.rect.x = (rightX + heroSpace + hero.rect.width > width) ? (width - heroSpace) : rightX + heroSpace;
-				if ((hero.rect.y === monster.rect.y) && ((monster.rect.x < hero.rect.x) && (rightX < monster.rect.x + heroSpace))) { 
-					// 高度一样，要画的x轴为英雄+40 在怪物的区间内【32， 72】就不能重写
+				if ((hero.rect.y === monster.rect.y) && ((monster.rect.x <= hero.rect.x) && (monster.rect.x >= hero.rect.x))) { 
+					// 高度一样，英雄要画的x轴为+40 在怪物的区间内【32， 72】就不能重写
 					hero.rect.x = rightX;
 				}
 			}
 			if (e.keyCode === 38) { // 向上移动
-				hero.rect.y =(hero.rect.y - heroSpace > 0) ?  (hero.rect.y - heroSpace) : 0;
+				let Yup = hero.rect.y;
+				hero.rect.y = (Yup - heroSpace > 0) ?  (hero.rect.y - heroSpace) : 0;
+				if ((hero.rect.x === monster.rect.x) && (monster.rect.y >= (hero.rect.y)) && (hero.rect.y >= monster.rect.y)) {
+					// 高度一样，英雄要画的y轴为+40 在怪物的区间内【32， 72】就不能重写
+					hero.rect.y = Yup;
+				}
 			}
 			if (e.keyCode === 40) { // 向下移动
 				let Ydown = hero.rect.y;
-				hero.rect.y = (hero.rect.y + heroSpace + hero.rect.height > height) ? (height - heroSpace) : hero.rect.y + heroSpace;
-				if ((monster.rect.y < hero.rect.y) && (hero.rect.y + heroSpace < monster.rect.y )) { 
-					// 高度一样，要画的x轴为英雄+40 在怪物的区间内【32， 72】就不能重写
+				hero.rect.y = ((Ydown + heroSpace + hero.rect.height) > height) ? (height - heroSpace) : hero.rect.y + heroSpace;
+				if ((hero.rect.x === monster.rect.x) && (monster.rect.y >= (hero.rect.y)) && (hero.rect.y >= monster.rect.y)) { 
+					// 高度一样，英雄要画的y轴为+40 在怪物的区间内【32， 72】就不能重写
 					hero.rect.y = Ydown;
 				}
 			}
@@ -199,29 +146,9 @@
 		monster.draw();
 	}
 
-	// function heroMove(rect){
-	// 	const width = 300; // 活动范围宽度
-	// 	const height = 500; // 活动范围高度
-	// 	document.onkeydown = (function(e) {
-	// 		// console.log('e.keyCode:', e.keyCode);
-	// 		// 37, 38, 39 ,40 方向键值
-	// 		if (e.keyCode === 37) { // 向左移动
-	// 			rect.x =(rect.x - 40 > 0) ?  (rect.x - 40) : 0;
-	// 		}
-	// 		if (e.keyCode === 39) { // 向右移动
-	// 			rect.x = (rect.x + 40 > width) ? width : rect.x + 40;
-	// 		}
-	// 		return rect;
-	// 	})
-	// }
-
-
 	var resourceManager = prepare();
 	resourceManager.getResource(function (context, heroImg, allSpriteImg) {
 		drawHero(context, heroImg, allSpriteImg);
-		// console.log("hero:", hero);
-		// const moveHero = heroMove(hero.rect);
-		// hero.draw();
 		});
 
 })()
